5bbb5a071d27e89da8c179188073ce92ab3d2b54
AhmedMoharam <33280932+AhmedMoharam@users.noreply.github.com>
2019-04-20 00:17:02 +0200

    calculator first commit
    adding calculator basic opeartions -- switch case is similar to if and else if

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
+//TODO: Create add, subtract, mul, divide functions here and fix the divide bug
+
+
 int main(){
-	printf("Hello githup\n");
+    int a,b;
+    char operation;
+    float result;
+	scanf("%i%c%i",&a,&operation,&b);
+	
+	switch (operation) {
+	    case '+':
+	        result = a+b; 
+	        break;
+	    case '-': 
+	        result = a-b;
+	        break;
+	    case '*': 
+	        result = a*b;
+	        break;
+	    case '/': 
+	        result = a/b;
+	        break;
+	    default : 
+	        printf("UnKnown Operation try +,-,* or / \n");
+	        result = -1;
+	        break;
+	}
+	
+	printf("%i %c %i = %.4f\n",a,operation,b,result);
 return 0;
 }
